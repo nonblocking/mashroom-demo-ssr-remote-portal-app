0ff225729d8fa1eccb233958a8090f24da5785fa
Jürgen Kofler <juergen.kofler@nonblocking.at>
2022-10-19 13:37:55 +0200

    fixed API URL

--- src/server/routes/api/randomJoke.ts
+++ src/server/routes/api/randomJoke.ts
@@ -5,20 +5,17 @@ import type { Request, Response } from 'express';
 
 export default async (req: Request, res: Response) => {
     try {
-        const result = await fetch('http://api.icndb.com/jokes/random');
+        const result = await fetch('https://api.chucknorris.io/jokes/random');
         if (!result.ok) {
+            logger.error(`Looking up a random joke failed. Status: ${result.status}`);
             res.sendStatus(500);
             return;
         }
 
         const data = await result.json();
-        if (data.type !== 'success') {
-            res.sendStatus(500);
-            return;
-        }
 
         res.json({
-            joke: data.value.joke
+            joke: data.value
         });
     } catch (e) {
         logger.error(e, 'Looking up a random joke failed');
